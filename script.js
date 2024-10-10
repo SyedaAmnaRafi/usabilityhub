695f99b34bb955207a34d2446afb6f4f97a12a1a
amnarafi <s.amnarafi1999@gmail.com>
2024-10-10 18:35:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// document.getElementById('nav-toggle').addEventListener('click', function() {
+//     const navLinks = document.getElementById('nav-links');
+//     navLinks.classList.toggle('active');
+
+    document.getElementById('nav-toggle').addEventListener('click', function() {
+        const navLinks = document.getElementById('nav-links');
+        navLinks.classList.toggle('active');
+      });
+      
